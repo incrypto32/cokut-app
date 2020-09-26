095f5957a0dfef124b5ca97d22b634f9c9aacab1
krishna-incrypto <vpkrishnanand@gmail.com>
2020-09-26 17:43:52 +0530

    fix typo

--- lib/presentation/switchers/app_view.dart
+++ lib/presentation/switchers/app_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:cokut/bloc_listener.dart/my_bloc_listener.dart';
+import 'package:cokut/bloc_listener/my_bloc_listener.dart';
 import 'package:cokut/cubit/authentication/authentication_cubit.dart';
 import 'package:cokut/cubit/firebase_app/firebase_app_cubit.dart';
 import 'package:cokut/cubit/login_form/login_cubit.dart';
