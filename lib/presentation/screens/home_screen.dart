2c446f003844d32860d1c173a5b872995a4ae0ed
krishna-incrypto <vpkrishnanand@gmail.com>
2020-09-27 17:54:15 +0530

    Added settings page

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -14,10 +14,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle = TextStyle(
-    fontSize: 30,
-    fontWeight: FontWeight.bold,
-  );
 
   static const List<Widget> _widgetOptions = <Widget>[
     HomeWidget(),
--- lib/presentation/widgets/main/settings_page.dart
+++ lib/presentation/widgets/main/settings_page.dart
@@ -5,23 +5,58 @@ class Settings extends StatelessWidget {
   const Settings();
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          FlatButton(
-            onPressed: () {
-              authService.signOut();
-            },
-            child: Text(
-              "Log Out",
-              style: TextStyle(color: Colors.black),
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 20,
             ),
-          ),
-        ],
+            ListTile(
+              leading: Icon(
+                Icons.person,
+                color: Colors.green,
+                size: 60,
+              ),
+              title: Text(
+                " Name",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              subtitle: Text("mail@mail.com"),
+              trailing: IconButton(
+                onPressed: authService.signOut,
+                icon: Icon(
+                  Icons.lock_outline,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            buildSettingsItemTile("Address", Icons.home),
+            buildSettingsItemTile("Payments", Icons.payment),
+            buildSettingsItemTile("Orders", Icons.event_note),
+            buildSettingsItemTile("Refferals", Icons.card_giftcard),
+            buildSettingsItemTile("Settings", Icons.settings),
+            Container(
+              color: Colors.grey[100],
+              height: 30,
+            ),
+            buildSettingsItemTile("Terms of Service", Icons.import_contacts),
+            buildSettingsItemTile("Rate the app", Icons.star),
+            buildSettingsItemTile("About", Icons.subject),
+          ],
+        ),
       ),
+    );
+  }
+
+  ListTile buildSettingsItemTile(String content, IconData icon) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(
+        content,
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      trailing: Icon(Icons.chevron_right),
     );
   }
 }
