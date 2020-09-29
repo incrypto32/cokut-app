2c677b6601deae6dd586bf81ebce31df8a57838f
krishna-incrypto <vpkrishnanand@gmail.com>
2020-09-29 18:58:40 +0530

    cart ui

--- lib/presentation/widgets/main/cart_page.dart
+++ lib/presentation/widgets/main/cart_page.dart
@@ -7,56 +7,104 @@ class CartWidget extends StatelessWidget {
   const CartWidget();
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CartCubit, CartState>(builder: (context, state) {
-      List<MealTile> mealTiles = [];
-      state.cartItems.forEach(
-        (key, value) => mealTiles.add(
-          MealTile(
-            meal: value.meal,
-          ),
-        ),
-      );
-      if (mealTiles.length == 0) {
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                // color: Colors.amber,
-                margin: EdgeInsets.symmetric(horizontal: 50),
-                child: Image.asset('assets/images/empty-cart.png'),
-              ),
-            ],
+    return BlocBuilder<CartCubit, CartState>(
+      builder: (context, state) {
+        List<MealTile> mealTiles = [];
+        state.cartItems.forEach(
+          (key, value) => mealTiles.add(
+            MealTile(
+              meal: value.meal,
+            ),
           ),
         );
-      }
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("Your Food Cart", style: Theme.of(context).textTheme.headline5),
-          Column(
-            children: mealTiles,
+        if (mealTiles.length == 0) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 50),
+                  child: Image.asset('assets/images/empty-cart.png'),
+                ),
+              ],
+            ),
+          );
+        }
+        return SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "Your Food Cart",
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                  ),
+                  ...mealTiles,
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 20,
+                    ),
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [Text("Cart Total"), Text("95.0")],
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [Text("Delivary Charge"), Text("95.0")],
+                          ),
+                        ),
+                        Divider(
+                          color: Colors.black54,
+                          thickness: 1,
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [Text("Grand Total"), Text("95.0")],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ]),
+                Container(
+                  margin: EdgeInsets.all(30),
+                  height: 40.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                  ),
+                  child: RaisedButton(
+                    color: Colors.green,
+                    child: Text(
+                      "PLACE ORDER",
+                      style: TextStyle(fontSize: 18.0, color: Colors.white),
+                    ),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
           ),
-          Expanded(child: Container()),
-          Container(
-            margin: EdgeInsets.all(30),
-            height: 40.0,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.green,
-            ),
-            child: RaisedButton(
-              color: Colors.green,
-              child: Text(
-                "PLACE ORDER",
-                style: TextStyle(fontSize: 18.0, color: Colors.white),
-              ),
-              onPressed: () {},
-            ),
-          )
-        ],
-      );
-    });
+        );
+      },
+    );
   }
 }
 
